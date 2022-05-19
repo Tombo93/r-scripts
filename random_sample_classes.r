478fd79c37aac16ab230115a876d30a026c57f11
Tom <tomliessmann@gmail.com>
2022-05-19 20:39:56 +0200

    .

--- random_sample_classes.r
+++ random_sample_classes.r
@@ -1,38 +1,40 @@
 library(dplyr)
+library(tidyverse)
 
-# TODO: Es muss noch dafür gesort werden,
-# dass man einen split angeben kann (z.B. c(0.6,0.4))
-# anstelle von einer sample_size
-sample_by_class <- function(df, group, sample_size, replace=FALSE) {
-  # df : ein dataframe
-  # group : hiernach soll gruppiert werden
-  # sample_size : das ist die sample größe pro Kategorie
-  grouped_samples <- 
-    df %>%
-    group_by(group) %>%
-    slice_sample(
-      n=sample_size,
-      replace=replace
+
+split_df <- function(df, train_p, test_p) {
+  split_vec = sample(
+    c(
+      rep(0, train_p * nrow(df)),
+      rep(1, test_p * nrow(df))
     )
-    return(grouped_samples)
+  )
+  train_df <- df[split_vec == 0, ]     
+  test_df <- df[split_vec == 1, ]
+  return(list("train" = train_df, "test" = test_df))
 }
 
-groups <- sample_by_class(BTK, Level_1, 100)
 
-# groups <-
-#   BTK %>%
-#   select(OBJECTID) %>%
-#   distinct(OBJECTID) %>%
-#   rowwise() %>%
-#   mutate(group = sample(
-#     c('train', 'test'),
-#     1,
-#     replace = TRUE,
-#     prob = c(0.6, 0.4)
-#   ))
+split_by_classes <- function(df, filter_col, classes) {
+  train_df <- df[FALSE,] # Generiert leeren dataframe
+  test_df <- df[FALSE,]
+  for (name in classes) {
+    class_df <- subset(df, name == filter_col)
+    sets <- split_df(class_df, 0.6, 0.4)
+    rbind(train_df, sets$train)
+    rbind(test_df, sets$test)
+  }
+  return(list(train_df, test_df))
+}
 
-#   BTK <- BTK %>%
-#     left_join(groups)
 
-#   BTK_train <- filter(BTK, group == 'train')
-#   BTK_test <- filter(BTK, group == 'test')
+labels <- c('A', 'A', 'A', 'B', 'B', 'A', 'B', 'A', 'A', 'A')
+data <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+df <- data.frame(labels, data)
+x <- split_by_classes(df, labels, list('A', 'B'))
+x
+
+train <- df %>% group_by(labels) %>% sample_frac(0.6, replace=FALSE)
+test <- anti_join(df, train, by=labels)
+train
+test
