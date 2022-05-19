1abe6c9a539429b8db5cc744a37870c6bbf1424d
Tom <tomliessmann@gmail.com>
2022-05-19 18:07:42 +0200

    .

--- random_sample_classes.r
+++ random_sample_classes.r
@@ -1,19 +1,23 @@
-install.packages("tidyverse")
-library(tidyverse)
 library(dplyr)
-data("iris")
 
-sample_by_class <- function(df, group, sample_size) {
-  # slice_sample erwartet einen dataframe
-  groups <- 
+# TODO: Es muss noch dafür gesort werden,
+# dass man einen split angeben kann (z.B. c(0.6,0.4))
+# anstelle von einer sample_size
+sample_by_class <- function(df, group, sample_size, replace=FALSE) {
+  # df : ein dataframe
+  # group : hiernach soll gruppiert werden
+  # sample_size : das ist die sample größe pro Kategorie
+  grouped_samples <- 
     df %>%
     group_by(group) %>%
     slice_sample(
       n=sample_size,
-      replace=FALSE, # FALSE, wenn Zeilen nicht 2-mal gesampelt werden sollen
+      replace=replace
     )
+    return(grouped_samples)
 }
-groups <- sample_by_class(BTK, OBJECTID, 100)
+
+groups <- sample_by_class(BTK, Level_1, 100)
 
 # groups <-
 #   BTK %>%
